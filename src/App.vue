0f6e2bf478f50c3e065ca98594c0219ca76edb5a
tanaka <lemo.nade.engineer.room@gmail.com>
2022-12-30 15:47:52 +0900

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>hello</h1>
+  <v-app>
+    <v-main>
+      <HelloWorld />
+    </v-main>
+  </v-app>
+</template>
+
+<script setup lang="ts">
+  import HelloWorld from '@/components/HelloWorld.vue'
+</script>
+
+<style lang="scss" scoped>
+h1 {
+  color: gold;
+}
+</style>
